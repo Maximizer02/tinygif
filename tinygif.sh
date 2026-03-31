da8fcc51492c41fff2f1acd6c24ab558690de331
Maximizer02 <max.beller@yahoo.com>
2026-04-01 00:00:28 +0200

    add caching

--- tinygif.sh
+++ tinygif.sh
@@ -3,5 +3,37 @@ if [ $# -ne 1 ]; then
 	printf "usage: tinygif file.mp4"
 	exit 1;
 fi
-ffmpeg -i "$1" -vf "[0:v]fps=12,select='mod(n,2)',scale='144:81',split [a][b];[a] palettegen=max_colors=28:reserve_transparent=0 [p];[b][p] paletteuse=new=1" "tiny.gif" && du "tiny.gif"
+
+input_file="$1";
+output_file="$input_file.gif"
+
+fps="12";
+scale="144:81"
+colors="28"
+
+color_palette="$XDG_CACHE_HOME/tinygif/$colors_$1.png";
+
+if [ ! -f "$color_palette" ]; then
+	echo "Cache miss, calculating color palette: $color_palette";
+ 	ffmpeg \
+		-hide_banner \
+		-loglevel error \
+		-y \
+		-i "$input_file" \
+		-vf "palettegen=max_colors=$colors:reserve_transparent=0" \
+		"$color_palette";
+else
+	echo "Cache hit, using color palette: $color_palette";
+fi
+
+ffmpeg \
+	-hide_banner \
+	-loglevel error \
+	-y \
+	-i "$input_file" \
+	-i "$color_palette" \
+	-filter_complex "[0]fps=$fps,select='mod(n,2)',scale='$scale'[a];[a][1] paletteuse=new=1" \
+	"$output_file";
+
+echo "The resulting GIF is $(du -hs $output_file | xargs) in size";
 
