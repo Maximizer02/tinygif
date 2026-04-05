ee13bfd57881bd919c3736f13075892558f67233
Maximizer02 <max.beller@yahoo.com>
2026-04-06 01:31:10 +0200

    improve installer

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-# Create cache directory
-if [ ! -d  "$XDG_CACHE_HOME/tinygif" ]; then
-	mkdir "$XDG_CACHE_HOME/tinygif";
-fi
+# Create cache directory if it does not exist
+[ ! -z "$XDG_CACHE_HOME" ] && CACHE_DIR="$XDG_CACHE_HOME/tinygif" || CACHE_DIR="$HOME/.cache/tinygif";
+[ ! -d  "$CACHE_DIR" ] && mkdir "$CACHE_DIR";
 
-# Move script to location in PATH
+# Fall back to another location if the preferred one does not exist
 BIN="$HOME/.local/bin";
-if [ ! -d "$BIN" ] ; then
-	BIN="/usr/local/bin";
-fi
+[ ! -d "$BIN" ] && BIN="/usr/local/bin";
+
+# Move script to chosen directory
 cp ./tinygif.sh "$BIN/tinygif";
+printf "tinygif was installed to \x1B[33m$BIN\x1B[0m\n";
